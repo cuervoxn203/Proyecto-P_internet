66688d85e55584eb4d28cebaa8b2f4c05bcaa990
Zucarito <diego.campos5701@alumnos.udg.mx>
2024-11-20 10:45:12 -0600

    correo

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,27 @@ use App\Http\Controllers\FormularioController;
 
 
 // Ruta para la vista de bienvenida
+use App\Http\Controllers\ConsultaController;
+use App\Http\Controllers\MailTestController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Ruta pública
 Route::get('/', function () {
     return view('welcome');
+
+
 });
 
 Route::resource('terapeutas', TerapeutaController::class);
-
 
 
 // Grupo de rutas protegidas por middleware de autenticación (Opcional)
@@ -30,3 +45,28 @@ Route::get('/terapeutas', [TerapeutaController::class, 'index'])->name('terapeut
 Route::resource('terapeutas', TerapeutaController::class)->parameters([
     'terapeutas' => 'terapeuta'
 ]);
+/* RUTAS PROTEGIDAS */
+Route::middleware([
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    // Ruta del dashboard
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    /* TERAPEUTAS VIEWS */
+    Route::resource('terapeutas', TerapeutaController::class);
+
+    /* FORMULARIOS VIEWS */
+    Route::resource('formularios', FormularioController::class)->parameters(['formularios' => 'formulario']);
+
+    /* CONSULTAS VIEWS */
+    Route::resource('consultas', ConsultaController::class);
+
+    Route::get('/test-email', [MailTestController::class, 'sendTestEmail']);
+
+});
+
+
+
